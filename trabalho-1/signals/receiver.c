3cf5264643865423d328a53eb94eb51a38939046
aline.capucho <aline.capucho@olxbr.com>
2023-04-18 23:51:41 -0300

    feat: add initial folder structure and signals functions

---
+++ trabalho-1/signals/receiver.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void signal_handler (int userSignal) {
+  switch(userSignal) {
+    case SIGUSR1:
+      printf("Sinal USR1 recebido com sucesso. \n");
+      break;
+    case SIGUSR2:
+      printf("Sinal USR2 recebido com sucesso. \n");
+      break;
+    case SIGTERM:
+      printf("Sinal TERM recebido com sucesso. O programa será finalizado.\n");
+      exit(0);
+  };
+  return;
+}
+
+int main() {
+  int wait_mode;
+
+  signal(SIGUSR1, signal_handler);
+  signal(SIGUSR2, signal_handler);
+  signal(SIGTERM, signal_handler);
+
+  printf("MODO DE ESPERA | CÓDIGO\n");
+  printf("--------------\n");
+  printf("Busy wait | 0\n");
+  printf("--------------\n");
+  printf("Blocking wait | 1\n");
+
+  printf("Escolha o modo de espera: \n");
+  scanf("%d", &wait_mode);
+
+  switch (wait_mode) {
+    case 0:
+      while(1) {
+        sleep(1);
+      }
+    case 1:
+      while(1) {
+        pause();
+      }
+    default:
+      printf("Modo de espera inválido.\n");
+      printf("Verifique e tente novamente.\n");
+      return 0;
+  };
+}
---
+++ trabalho-1/signals/sender.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+#include "receiver.c"
+
+int main() {
+  int pid, signal;
+
+  printf("MODO | CÓDIGO\n");
+  printf("--------------\n");
+  printf("SIGTERM | %d\n", SIGTERM);
+  printf("--------------\n");
+  printf("SIGUSR1 | %d\n", SIGUSR1);
+  printf("--------------\n");
+  printf("SIGUSR2 | %d\n", SIGUSR2);
+  printf("Para demais códigos consulte: https://man7.org/linux/man-pages/man7/signal.7.html\n");
+
+  printf("\nDigite o PID do processo: \n");
+  scanf("%d", &pid);
+  printf("\nDigite o código do sinal: \n");
+  scanf("%d", &signal);
+
+  int sucess_code = kill(pid, signal);
+
+  if (sucess_code < 0 ) {
+    switch (errno) {
+      case 'EINVAL':
+        printf("\nSinal inexistente.");
+        break;
+      case 'EPERM':
+        printf("\nOperação não permitida.");
+        break;
+      case 'ESRCH':
+        printf("\nProcesso inexistente.");
+        break;
+      default:
+        printf("Erro. Verifique e tente novamente.");
+        break;
+    }
+    return 0;
+  } else {
+    printf("Sinal enviado ao processo com sucesso.");
+    return 1;
+  };
+}
